cbd32bcd96ef0f1cbb25982ffd8e8b31737f3f85
SkynetDev <andrejohnson0017@gmail.com>
2020-12-27 23:21:30 -0500

    added token burn function

--- Inheritance.sol
+++ Inheritance.sol
@@ -16,4 +16,21 @@ contract inheritanceModifier {
         require(msg.sender == owner, "You are not the owner");
         tokenBalance[owner]++;
     }
+    
+    function burnToken() public {
+        require(msg.sender == owner, "You are not the owner");
+        tokenBalance[owner]--;
+    }
+    
+    function purchaseToken() public payable {
+        require((tokenBalance[owner] * tokenPrice) / msg.value > 0, "not enough funds");
+        tokenBalance[owner] -= msg.value / tokenPrice;
+        tokenBalance[msg.sender] += msg.value / tokenPrice;
+    }
+    
+    function sendToken(address _to, uint _amount) public {
+        require(tokenBalance[msg.sender] >= _amount, "You do not have enough funds");
+        assert(tokenBalance[_to] +_amount >= tokenBalance[_to]);
+        assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
+    }
 }
